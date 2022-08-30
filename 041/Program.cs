35eb38f994e32af6973a52d881cbec7c4dbc5f73
Aleksandr <budkin86bk@yandex.ru>
2022-08-30 17:59:29 +0500

    Практическое задание к семинару 6

---
+++ 041/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Введите количество чисел (M): ");
+int mNumbers = Convert.ToInt32(Console.ReadLine());
+int [] numbers = new int [mNumbers]; 
+Console.Write("Введите числа (M): "); 
+
+for (int i = 0; i < mNumbers; i++)
+{
+    numbers[i] = Convert.ToInt32(Console.ReadLine());
+}    
+Console.WriteLine($"Числа пользователя: [{String.Join(": ", numbers)}]");
+
+int count = 0;
+for(int i = 0; i < mNumbers; i++)
+{
+    if(numbers[i] > 0) count++;
+}
+Console.WriteLine($"Кол-во чисел больше 0: {count}");
+
+
---
+++ Dopl/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите десятичное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int [] binarySystem = new int [10];
+binarySystem[0] = 1;
+for (int i = 1; i < 10; i++)
+{
+    binarySystem[i] = binarySystem[i - 1] * 2;
+}
+
+int [] reversBinarySystem = new int [10];
+for (int i = 0; i < 10; i++)
+{
+    reversBinarySystem[i] = binarySystem [9 - i];
+}
+Console.WriteLine($"[{String.Join(", ", reversBinarySystem )}]");
+
+//Дальше не знаю) 
